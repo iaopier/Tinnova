6a64ab440091d6fc85a415b3720566ff3bfff08a
Piercarlo <pierhett@gmail.com>
2020-07-14 15:04:07 -0300

    Model Veículo baseado na descrição criado. Adicionado arquivo docker.

---
+++ TinnovaAPI/src/main/java/com/tinnova/veiculos/Model/Veiculo.java
@@ -0,0 +1,106 @@
+package com.tinnova.veiculos.Model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "TB_VEICULO")
+public class Veiculo {
+	@Column
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long idProduto;
+	@Column
+	private String veiculo;
+	@Column
+	private String marca;
+	@Column
+	private int ano;
+	@Column
+	@Lob
+	private String descricao;
+	@Column
+	private boolean vendido;
+	@Column
+	private Date created;
+	@Column
+	private Date updated;
+
+	public Veiculo(String veiculo, String marca, int ano, String descricao, boolean vendido, Date created,
+			Date updated) {
+		super();
+		this.veiculo = veiculo;
+		this.marca = marca;
+		this.ano = ano;
+		this.descricao = descricao;
+		this.vendido = vendido;
+		this.created = created;
+		this.updated = updated;
+	}
+
+	public String getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(String veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isVendido() {
+		return vendido;
+	}
+
+	public void setVendido(boolean vendido) {
+		this.vendido = vendido;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+}
---
+++ TinnovaAPI/src/main/java/com/tinnova/veiculos/TinnovaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tinnova.veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinnovaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinnovaApiApplication.class, args);
+	}
+
+}
